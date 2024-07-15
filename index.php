eeee1c1dd7007a5cd9394c56c2b095872285c78a
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-07-15 19:03:23 +0200

    Refactor index

--- index.php
+++ index.php
@@ -10,70 +10,68 @@ include_once 'view.php';
 $view = new View();
 $controller = new Controller();
 
-// Adult
-if(isset($_GET['page']) && $_GET['page'] === 'adult_form') {
-    $controller->showAdultForm();
-}
-
-elseif (isset($_GET['page']) && $_GET['page'] === 'adult_post') {
-    $controller->submitAdultForm($_POST);
-}
-
-elseif (isset($_SESSION['adultForm']) && isset($_GET['page']) && $_GET['page'] === 'adult_list') {
-        $adultList = [];
-        for ($i = 0; $i < count($_SESSION['adultForm']); $i++) {
-            $adultList[] = unserialize($_SESSION['adultForm'][$i]);
-        }
-        $view->printAdultList($adultList);
-}
-
-elseif (isset($_GET['page']) && $_GET['page'] === 'adult_list') {
-    $view->showEmpty();
-}
-
-// Teenager
-elseif (isset($_GET['page']) && $_GET['page'] === 'teen_form') {
-    $controller->showTeenForm();
-}
-
-elseif (isset($_GET['page']) && $_GET['page'] === 'teen_post') {
-    $controller->submitTeenForm($_POST);
-}
-
-elseif (isset($_SESSION['teenForm']) && isset($_GET['page']) && $_GET['page'] === 'teen_list') {
-    $teenList = [];
-    for ($i = 0; $i < count($_SESSION['teenForm']); $i++) {
-        $teenList[] = unserialize($_SESSION['teenForm'][$i]);
-    }
-    $view->printTeenList($teenList);
-}
-
-elseif (isset($_GET['page']) && $_GET['page'] === 'teen_list') {
-    $view->showEmpty();
-}
-
-// Child
-elseif (isset($_GET['page']) && $_GET['page'] === 'child_form') {
-    $controller->showChildForm();
-}
-
-elseif (isset($_GET['page']) && $_GET['page'] === 'child_post') {
-    $controller->submitChildForm($_POST);
-}
-
-elseif (isset($_SESSION['childForm']) && isset($_GET['page']) && $_GET['page'] === 'child_list') {
-    $childList = [];
-    for ($i = 0; $i < count($_SESSION['childForm']); $i++) {
-        $childList[] = unserialize($_SESSION['childForm'][$i]);
-    }
-    $view->printChildList($childList);
-}
-
-elseif (isset($_GET['page']) && $_GET['page'] === 'child_list') {
-    $view->showEmpty();
-}
-
-// Home
-else {
+if (empty($_GET['page'])) {
     $view->showHome();
+}
+
+switch ($_GET['page']) {
+    case 'adult_form':
+        $controller->showAdultForm();
+        break;
+    case 'adult_post':
+        $controller->submitAdultForm($_POST);
+        break;
+    case 'adult_list':
+        if (isset($_SESSION['adultForm'])) {
+            $adultList = [];
+
+            for ($i = 0; $i < count($_SESSION['adultForm']); $i++) {
+                $adultList[] = unserialize($_SESSION['adultForm'][$i]);
+            }
+
+            $view->printAdultList($adultList);
+        } else {
+            $view->showEmpty();
+        }
+        break;
+    case 'teen_form':
+        $controller->showTeenForm();
+        break;
+    case 'teen_post':
+        $controller->submitTeenForm($_POST);
+        break;
+    case 'teen_list':
+        if (isset($_SESSION['teenForm'])) {
+            $teenList = [];
+
+            for ($i = 0; $i < count($_SESSION['teenForm']); $i++) {
+                $teenList[] = unserialize($_SESSION['teenForm'][$i]);
+            }
+
+            $view->printTeenList($teenList);
+        } else {
+            $view->showEmpty();
+        }
+        break;
+    case 'child_form':
+        $controller->showChildForm();
+        break;
+    case 'child_post':
+        $controller->submitChildForm($_POST);
+        break;
+    case 'child_list':
+        if (isset($_SESSION['childForm'])) {
+            $childList = [];
+
+            for ($i = 0; $i < count($_SESSION['childForm']); $i++) {
+                $childList[] = unserialize($_SESSION['childForm'][$i]);
+            }
+
+            $view->printChildList($childList);
+        } else {
+            $view->showEmpty();
+        }
+        break;
+    default:
+        $view->showHome();
 }
